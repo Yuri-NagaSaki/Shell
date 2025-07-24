2ee6fe66c66b85e399535de046e83776dda090c9
Firefly <77047823+Yuri-NagaSaki@users.noreply.github.com>
2025-07-24 15:14:14 +0800

    Update setup_serverstatus.sh

--- setup_serverstatus.sh
+++ setup_serverstatus.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 仅下载 install-rust_serverstatus.sh
-curl -sS -O https://raw.githubusercontent.com/Yuri-NagaSaki/Shell/refs/heads/main/install-rust-serverstatus.sh
-chmod +x install-rust-serverstatus.sh
+curl -sS -O https://raw.githubusercontent.com/Yuri-NagaSaki/Shell/refs/heads/main/install_rust-serverstatus.sh
+chmod +x install-rust_serverstatus.sh
 
 # 下载并执行 serverstatus_manager.sh
 curl -sS -O https://raw.githubusercontent.com/Yuri-NagaSaki/Shell/refs/heads/main/serverstatus_manager.sh
